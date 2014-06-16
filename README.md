6b2b91d09cb50e383ff0276c58937b7c3f1b24dd
xiebaochun <463449055@qq.com>
2014-06-16 09:27:13 +0800

    upload

---
+++ README.md
@@ -0,0 +1,16 @@
+Same Android project on how to implement an image filter in C using the Android NDK.
+
+Use the slider to select a brightness value that will be bounded from 1-10 and then click on the button to apply the brightness filter.
+
+I wrote about this at: http://ruckus.tumblr.com/post/18055652108/writing-a-basic-image-filter-in-android-using-ndk
+
+Screenshots from before and after:
+
+### Before
+![image1][]
+
+### After
+![image2][]
+
+[image1]: http://codycaughlan.s3.amazonaws.com/images/before.png
+[image2]: http://codycaughlan.s3.amazonaws.com/images/after.png
---
+++ src/com/example/ImageActivity.java
@@ -0,0 +1,66 @@
+package com.example;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class ImageActivity extends Activity {
+  private ImageView imageView;
+  private Bitmap bitmap;
+  private Button button;
+  private SeekBar seekBar;
+  private float brightness;
+  private TextView label;
+  private Bitmap original;
+
+  static {
+    System.loadLibrary("imageprocessing");
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    original = BitmapFactory.decodeResource(getResources(), R.drawable.wallace);
+
+    imageView = (ImageView) findViewById(R.id.imageView);
+    button = (Button) findViewById(R.id.button);
+    button.setText("更新亮度");
+    seekBar = (SeekBar) findViewById(R.id.seekBar);
+    label = (TextView) findViewById(R.id.label);
+
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        adjustBrightness();
+      }
+    });
+
+    seekBar.setOnSeekBarChangeListener( new SeekBar.OnSeekBarChangeListener() {
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        brightness = (float)(progress / 10.0f);
+        label.setText("Brightness = " + brightness);
+        adjustBrightness();
+      }
+
+      public void onStartTrackingTouch(SeekBar seekBar) {}
+      public void onStopTrackingTouch(SeekBar seekBar){
+    	  
+      }
+    });    
+  }
+
+  private void adjustBrightness() {
+    Bitmap bitmap = original.copy(Bitmap.Config.ARGB_8888, true);
+    brightness(bitmap, brightness);
+    imageView.setImageBitmap(bitmap);
+  }
+
+  public native void brightness(Bitmap bmp, float brightness);
+}
